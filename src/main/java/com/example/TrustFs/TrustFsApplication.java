6816d6613f212787d55233c8377d8a52694ed435
Bhande Hemant <bhandehemant2004@gmail.com>
2026-05-21 15:14:55 +0000

    basic file structure created

---
+++ src/main/java/com/example/TrustFs/TrustFsApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrustFs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustFsApplication.class, args);
+	}
+
+}
